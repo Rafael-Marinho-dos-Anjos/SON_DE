8d3c823dc7537ba15708c9f54ab748ec85844027
Rafael-Marinho-dos-Anjos <rafaelmarinhoanjos@gmail.com>
2024-10-31 15:55:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reprodução do artigo: Self-organizing neighborhood-based differential evolution for global optimization
+
+### Projeto para a disciplina de Computação Evolucionária do programa de pós-graduação em Ciências da Computação - UFPE
+
+## Alunos:
+- João Pedro Alves Maciel
+- Juan Alberto Rojas Tueros
+- Rafael Marinho dos Anjos
+
+## Orientador: Aluizio Fausto Ribeiro Araújo
---
+++ src/utils/distances.py
@@ -0,0 +1,81 @@
+""" Distance functions used by SOM
+"""
+
+import numpy as np
+
+
+def euclidean_squared(a: np.ndarray, b: np.ndarray):
+    """ 
+    Euclidean distance squared
+    
+    Params:
+        a: ndarray: First input vector
+        b: ndarray: Seccond input vector
+    
+    Return: The euclidean distance squared between a and b vectors
+    Return type: float | ndarray
+    """
+    dist = a - b
+    dist = np.square(dist)
+    dist = np.sum(dist, axis=-1)
+
+    return dist
+
+
+def euclidean(a: np.ndarray, b: np.ndarray):
+    """ 
+    Euclidean distance
+    
+    Params:
+        a: ndarray: First input vector
+        b: ndarray: Seccond input vector
+    
+    Return: The euclidean distance between a and b vectors
+    Return type: float | ndarray
+    """
+    dist = euclidean(a, b)
+    dist = np.sqrt(dist)
+
+    return dist
+
+
+def manhattan(a: np.ndarray, b: np.ndarray):
+    """ 
+    Manhattan's distance
+    
+    Params:
+        a: ndarray: First input vector
+        b: ndarray: Seccond input vector
+    
+    Return: The manhattan's distance between a and b vectors
+    Return type: float | ndarray
+    """
+    dist = a - b
+    dist = np.abs(dist)
+    dist = np.sum(dist, axis=-1)
+
+    return dist
+
+
+def cosine(a: np.ndarray, b: np.ndarray):
+    """ 
+    Cosine distance
+    
+    Params:
+        a: ndarray: First input vector
+        b: ndarray: Seccond input vector
+    
+    Return: The cosine distance between a and b vectors
+    Return type: float | ndarray
+    """
+    dist = np.diag(np.dot(a, b.T))
+    norms = np.linalg.norm(a) * np.linalg.norm(b)
+
+    return dist / norms
+
+
+if __name__ == "__main__":
+    a = np.array([[1, 2, 3], [2, 3, 4]])
+    b = np.array([[1, 2, 3], [2, 3, 4]])
+
+    print(cosine(a, b))
