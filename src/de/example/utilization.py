ff1ab477c165cfd650630aa4470f5074dcd32214
Rafael-Marinho-dos-Anjos <rafaelmarinhoanjos@gmail.com>
2024-12-02 13:09:48 -0300

    Refactor: Organizando arquivos de exemplo

--- src/de/example/utilization.py
+++ src/de/example/utilization.py
@@ -5,52 +5,45 @@ from src.de.de_model import DE
 from src.utils.initialization import random
 from src.utils.mutation import de_rand_1
 from src.utils.crossing import binary
+from src.benchmark.functions import sphere
 
 
-# Definição de uma função de aptidão de uma hiperesfera de raio 3
-def fitness(input):
-    return np.abs(np.sum(input ** 2) - 9)
-
+dim = 20
 # DE com 20 induvíduos
 de = DE(
-    dim=3,
-    NP=50,
-    init_method=lambda x: random(x, [[-20, 20]]),
+    dim=dim,
+    NP=100,
+    init_method=lambda x: random(x, [[-80, 80]]),
     is_maximization=False
 )
+
+global_optmium = np.random.randint(-80, 80, size=dim)
 
 # Definindo a distância utilizada como Manhattan,
 # uma função de vizinhança gaussiana e um passo
 # de aprendizado inicial de 0.5
-cr_0 = 0.5
-f_0 = 0.9
+cr_0 = 0.9
+f_0 = 0.5
 de.attach(
     {
         "mutation": de_rand_1,
         "crossing": binary(cr_0),
-        "fitness": fitness,
+        "fitness": sphere(global_optmium),
         "F": f_0
     }
 )
 
 # Ajuste da população
-GEN_MAX = 25
+GEN_MAX = 1000
 for gen in range(GEN_MAX):
     de.new_generation()
-
-    cr = cr_0 * np.exp(-gen)
-    f = f_0 * np.exp(-gen)
-
-    de.attach(
-        {
-            "crossing": binary(cr_0),
-            "F": f_0
-        }
-    )
     
     # Visualização dos resultados
     best = de.best_individual_index()
     pop = de.get_population()
     fit = de.get_pop_fitness()
 
-    print(f"Melhor Individuo: {pop[best]}\tFitness: {fit[best]}")
+    print(f"t: {gen+1}\tMelhor Individuo: {pop[best]}\tFitness: {float(fit[best]):.2f}")
+
+print(f"Global optimum: {global_optmium}")
+
