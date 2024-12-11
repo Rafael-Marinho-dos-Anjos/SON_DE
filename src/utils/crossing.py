f0d28323889cf4365f212a297553fbff8d50e952
Rafael-Marinho-dos-Anjos <rafaelmarinhoanjos@gmail.com>
2024-12-11 17:15:38 -0300

    Feat: random operators

--- src/utils/crossing.py
+++ src/utils/crossing.py
@@ -20,5 +20,26 @@ def binary(CR: float):
     return __crossing
 
 
+def binary_random(CR: tuple[float, float]):
+    def __crossing(a: np.ndarray, b: np.ndarray, **kwargs):
+        j = np.random.randint(0, len(a))
+        trial = np.zeros(len(a))
+
+        min_cr = min(CR)
+        delta_cr = abs(CR[0] - CR[1])
+
+        CR = min_cr + np.random.rand() * delta_cr
+
+        for i in range(len(a)):
+            if i == j or np.random.rand() <= CR:
+                trial[i] = b[i]
+            else:
+                trial[i] = a[i]
+
+        return trial
+
+    return __crossing
+
+
 pop = np.arange(100)
 pop.resize((25, 4))
--- src/utils/mutation.py
+++ src/utils/mutation.py
@@ -85,6 +85,24 @@ def proposed_rand_1(pop: np.ndarray, f: float, *args, **kwargs):
     return xr1 + f * (xr2 - xr3)
 
 
+def proposed_f_rand_1(pop: np.ndarray, f: tuple[float, float], *args, **kwargs):
+    cluster = kwargs["cluster"]
+    alternative_1 = kwargs["alternative_1"]
+    alternative_2 = kwargs["alternative_2"]
+
+    if len(cluster) < 3:
+        cluster = np.concatenate((cluster, alternative_1), axis=0)
+    if len(cluster) < 3:
+        cluster = np.concatenate((cluster, alternative_2), axis=0)
+
+    xr1, xr2, xr3 = cluster[np.random.choice([i for i in range(len(cluster))], 3, replace=False)]
+
+    min_f = min(f)
+    delta_f = abs(f[0] - f[1])
+
+    return xr1 + (min_f + np.random.rand() * delta_f) * (xr2 - xr3)
+
+
 if __name__ == "__main__":
     pop = np.arange(100)
     pop.resize((25, 4))
