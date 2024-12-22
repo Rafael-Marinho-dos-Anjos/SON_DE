49a5764002829d0e66b81b891573a77b34830813
Rafael-Marinho-dos-Anjos <rafaelmarinhoanjos@gmail.com>
2024-12-22 14:02:14 -0300

    Feat: running to compare

---
+++ running_son_de.py
@@ -0,0 +1,88 @@
+import os
+import json
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from src.son_de.son_de_model import SON_DE
+from src.utils.initialization import lhs as initialization # Seleção do método de inicialização dos pesos do SOM
+from src.benchmark.functions import sphere as fit_function # Seleção da função de fitness
+
+PATH = "src/son_de/runs/runs.json"
+if os.path.exists(PATH):
+    with open(PATH, "r") as file:
+        runs = json.loads(file.read())
+else:
+    runs = dict()
+
+func_name = "F1"
+runs[func_name] = list()
+
+# Número de dimensões do indivíduo
+dim=30
+
+# Função de aptidão
+global_optimum = np.random.randint(-80, 80, size=30)
+fitness = fit_function(global_optimum)
+print(f"Ótimo global: {global_optimum}")
+
+def update_adjust(i, t, T, NP):
+    return 1 - ((t * NP + i) / (T * NP))
+
+# DE com 100 induvíduos, utilizada a distribuição do CEC [-80, 80]
+sigma_0 = 5
+tau_0 = 0.9
+SOM_EPOCHS = 25
+delta = 1
+r_min_max = (5, 70)
+
+for run in range(30):
+    best_fit = None
+    runs[func_name].append({
+        "fitness": list(),
+        "epoch": list()
+    })
+
+    son_de = SON_DE(
+        dim=30,
+        NP=100,
+        fitness_func=fitness,
+        topology_shape=(10, 10),
+        init_method=lambda x: initialization(x, [[-100, 100]]),
+        is_maximization=False,
+        reset_prototypes=True,
+        sigma0=sigma_0,
+        tau0=tau_0,
+        adjust=update_adjust
+    )
+
+    f_0 = 0.5
+    son_de.attach_de(
+        {
+            "F": f_0
+        }
+    )
+
+    best_fit_per_epoch = []
+    # Ajuste da população
+    GEN_MAX = 3000
+    for gen in tqdm(range(GEN_MAX)):
+        print(f"run [{run + 1}/30]")
+        son_de.new_gen(SOM_EPOCHS, delta, r_min_max)
+
+        best = son_de.best_individual_index()
+        fit = son_de.get_pop_fitness()
+        # Visualização dos resultados
+        if (gen + 1) % 1 == 0:
+            pop = son_de.get_population()
+
+            print(f"Ger: {gen+1}\tFitness: {fit[best]}")
+        
+        if best_fit is None or fit[best] < best_fit:
+            runs[func_name][-1]["fitness"].append(fit[best])
+            runs[func_name][-1]["epoch"].append(gen + 1)
+            best_fit = fit[best]
+
+with open(PATH, "w") as file:
+    file.write(json.dumps(runs))
