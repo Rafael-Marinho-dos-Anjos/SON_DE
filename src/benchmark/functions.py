423b7c6a16f6892afb25a0d74c5756a033ff74f3
Rafael Marinho <rafaelmarinhoanjos@gmail.com>
2024-12-30 22:18:48 -0300

    "Atualizando testes rodados"

--- src/benchmark/functions.py
+++ src/benchmark/functions.py
@@ -44,7 +44,9 @@ def rotated_bent_cigar(o: np.ndarray, **kwargs):
     def __function(x: np.ndarray):
         z = np.dot(m, x - o).astype(np.float64)
         res = z[0] ** 2 + 10 ** 6 * np.sum(z[1:] ** 2, axis=0)
+
         return res
+
     return __function
 
 
@@ -69,7 +71,9 @@ def rotated_rosenbrock(o: np.ndarray, **kwargs):
         z = (x - o).astype(np.float64) / 2.048
         z = np.dot(m, z) + 1
         res = np.sum(100 * (z[:-1]**2 - z[1:])**2 + (z[:-1] - 1)**2, axis=0)
+
         return res
+
     return __function
 
 
@@ -83,7 +87,9 @@ def rotated_schaffer_f7(o: np.ndarray, **kwargs):
         y = np.dot(m, np.matmul(lambda_, (x - o))).astype(np.longdouble)
         z = np.sqrt(y[:-1]**2 + y[1:]**2)
         res = np.sum(np.sqrt(z) * (1 + 0.001 * np.sin(50 * z)**2), axis=0)
+
         return res
+
     return __function
 
 
@@ -99,7 +105,9 @@ def rotated_ackley(o: np.ndarray, **kwargs):
         term1 = -20 * np.exp(-0.2 * np.sqrt(np.sum(z**2, axis=0) / D))
         term2 = -np.exp(np.sum(np.cos(2 * np.pi * z), axis=0) / D)
         res = term1 + term2 + 20 + np.e
+    
         return res
+
     return __function
 
 
@@ -142,7 +150,9 @@ def rastrigin(o: np.ndarray, **kwargs):
     def __function(x: np.ndarray):
         z = x - o
         res = np.sum(z**2 - 10 * np.cos(2 * np.pi * z) + 10, axis=0)
+
         return res
+
     return __function
 
 
@@ -153,7 +163,9 @@ def rotated_rastrigin(o: np.ndarray, **kwargs):
     def __function(x: np.ndarray):
         z = np.dot(m, np.diag(alpha) @ (x - o))
         res = np.sum(z**2 - 10 * np.cos(2 * np.pi * z) + 10, axis=0)
+
         return res
+
     return __function
 
 
@@ -165,7 +177,9 @@ def non_continuous_rotated_rastrigin(o: np.ndarray, **kwargs):
         y = np.where(np.abs(x - o) > 0.5, np.round(x - o), x - o)
         z = np.dot(m, np.diag(alpha) @ y)
         res = np.sum(z**2 - 10 * np.cos(2 * np.pi * z) + 10, axis=0)
+
         return res
+
     return __function
 
 
@@ -185,7 +199,8 @@ def schwefel(o: np.ndarray, **kwargs):
         c = c * (np.abs(x) <= 500)
 
         return a + b + c
-    
+
+
     def __function(x: np.ndarray):
         z = 10 * (x - o)
         z = np.matmul(lambda_, x) + 420.968746227503
@@ -257,7 +272,9 @@ def lunacek_bi_rastrigin(o: np.ndarray, mu0: float = 2.5, d: float = 1, **kwargs
         term2 = d * D + np.sum((z - mu1)**2, axis=0)
         term3 = 10 * np.sum(1 - np.cos(2 * np.pi * (z - mu0)), axis=0)
         res = np.minimum(term1, term2) + term3
+
         return res
+
     return __function
 
 
@@ -272,7 +289,9 @@ def rotated_lunacek_bi_rastrigin(o: np.ndarray, mu0: float = 2.5, d: float = 1, 
         term2 = d * D + np.sum((z - mu1)**2, axis=0)
         term3 = 10 * np.sum(1 - np.cos(2 * np.pi * (z - mu0)), axis=0)
         res = np.minimum(term1, term2) + term3
+
         return res
+
     return __function
 
 
@@ -281,7 +300,9 @@ def expanded_griewank_rosenbrock(o: np.ndarray, **kwargs):
         z = x - o + 1
         term1 = 100 * (z[:-1]**2 - z[1:])**2 + (z[:-1] - 1)**2
         res = np.sum(term1 / 4000 - np.prod(np.cos(term1 / np.sqrt(np.arange(1, term1.size + 1)))) + 1)
+
         return res
+
     return __function
 
 
@@ -292,33 +313,41 @@ def rotated_expanded_schaffer_f6(o: np.ndarray, **kwargs):
         z = np.dot(m, x - o)
         term = 0.5 + (np.sin(np.sqrt(z[:-1]**2 + z[1:]**2))**2 - 0.5) / (1 + 0.001 * (z[:-1]**2 + z[1:]**2))
         res = np.sum(term, axis=0)
+
         return res
+
     return __function
 
 
 # composition functions:
 
-def calculate_weights(z: np.ndarray, o: np.ndarray, sigma: float):
-    w = np.exp(-np.sum((z - o)**2, axis=0) / (2 * sigma**2))
+def calculate_weights(x: np.ndarray, o: np.ndarray, sigma: float):
+    if (x != o).sum() == 0:
+        return 1
+    
+    D = o.shape[0]
+    w = np.exp(-np.sum((x - o)**2, axis=0) / (2 * D * sigma**2))
+    w = w / np.sqrt(np.sum((x - o)**2))
     w /= np.sum(w)
+
     return w
 
 
 def composition_function_any(sigmas: list, lambdas: list, biases: list, functions: list):
-    functions = functions
-    
-    def __composition_function(o: np.ndarray, m: list):
-        functions = [func(o=o, m=m[i]) for i, func in enumerate(functions)]
+    __functions = functions
+    def __composition_function(o: np.ndarray, **kwargs):
+        functions = [func(o=o, **kwargs) for i, func in enumerate(__functions)]
 
         def __function(x: np.ndarray):
             results = []
+            w_acc = 0
             for i in range(len(functions)):
-                z = (x - o[i]) / lambdas[i]
-                z = np.dot(m[i], z)
-                f_i = functions[i](z)
-                results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
+                f_i = functions[i](x)
+                w = calculate_weights(x, o, sigmas[i])
+                w_acc += w
+                results.append((lambdas[i] * f_i + biases[i]) * w)
 
-            res = np.sum(results, axis=0)
+            res = np.sum(results, axis=0) / w_acc
             return res
         
         return __function
@@ -326,107 +355,102 @@ def composition_function_any(sigmas: list, lambdas: list, biases: list, function
     return __composition_function
 
 
-def composition_function_1(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
+composition_function_1 = composition_function_any(
+    sigmas=[10, 20, 30, 40, 50],
+    lambdas=[1, 1e-6, 1e-26, 1e-6, 0.1],
+    biases=[0, 100, 200, 300, 400],
+    functions=[
+        rotated_rosenbrock,
+        different_powers,
+        rotated_bent_cigar,
+        rotated_discus,
+        sphere
+    ]
+)
 
+composition_function_2 = composition_function_any(
+    sigmas=[20, 20, 20],
+    lambdas=[1, 1, 1],
+    biases=[0, 100, 200],
+    functions=[
+        schwefel,
+        schwefel,
+        schwefel
+    ]
+)
 
-def composition_function_2(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
+composition_function_3 = composition_function_any(
+    sigmas=[20, 20, 20],
+    lambdas=[1, 1, 1],
+    biases=[0, 100, 200],
+    functions=[
+        rotated_schwefel,
+        rotated_schwefel,
+        rotated_schwefel
+    ]
+)
 
+composition_function_4 = composition_function_any(
+    sigmas=[20, 20, 20],
+    lambdas=[0.25, 1, 0.25],
+    biases=[0, 100, 200],
+    functions=[
+        rotated_schwefel,
+        rotated_rastrigin,
+        rotated_weierstrass
+    ]
+)
 
-def composition_function_3(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
+composition_function_5 = composition_function_any(
+    sigmas=[10, 30, 50],
+    lambdas=[0.25, 1, 0.25],
+    biases=[0, 100, 200],
+    functions=[
+        rotated_schwefel,
+        rotated_rastrigin,
+        rotated_weierstrass
+    ]
+)
 
+composition_function_6 = composition_function_any(
+    sigmas=[10, 10, 10, 10, 10],
+    lambdas=[ 0.25, 1, 1e-7, 2.5, 10],
+    biases=[0, 100, 200, 300, 400],
+    functions=[
+        rotated_schwefel,
+        rotated_rastrigin,
+        rotated_high_conditioned_elliptic,
+        rotated_weierstrass,
+        rotated_griewank
+    ]
+)
 
-def composition_function_4(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
+composition_function_7 = composition_function_any(
+    sigmas=[10, 10, 10, 10, 10],
+    lambdas=[100, 10, 2.5, 25, 0.1],
+    biases=[0, 100, 200, 300, 400],
+    functions=[
+        rotated_griewank,
+        rotated_rastrigin,
+        rotated_schwefel,
+        rotated_weierstrass,
+        sphere,
+    ]
+)
 
+composition_function_8 = composition_function_any(
+    sigmas=[10, 20, 30, 40, 50],
+    lambdas=[2.5, 2.5e-3, 2.5, 5e-4,0.1],
+    biases=[0, 100, 200, 300, 400],
+    functions=[
+        # func ainda nao implementada,
+        rotated_schaffer_f7,
+        rotated_schwefel,
+        rotated_expanded_schaffer_f6,
+        sphere,
+    ]
+)
 
-def composition_function_5(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
-
-
-def composition_function_6(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
-
-
-def composition_function_7(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
-
-
-def composition_function_8(o: np.ndarray, M: list, sigmas: list, lambdas: list, biases: list, functions: list):
-    def __function(x: np.ndarray):
-        results = []
-        for i in range(len(functions)):
-            z = (x - o[i]) / lambdas[i]
-            z = np.dot(M[i], z)
-            f_i = functions[i](z)
-            results.append((f_i + biases[i]) * calculate_weights(z, o[i], sigmas[i]))
-        res = np.sum(results, axis=0)
-        return res
-    return __function
 
 if __name__ == "__main__":
     k = np.arange(1, 21)
--- visualize_results.py
+++ visualize_results.py
@@ -7,10 +7,14 @@ with open("src/proposed_som_de/runs/runs.json", "r") as file:
 
 gambiarra = 10 ** 100
 
-for func in runs.keys():
+for i in range(1, 29):
+    func = "F" + str(i)
+    if func not in runs.keys():
+        continue
+
     best_fits = list()
     for run in runs[func]:
         best_fits.append(run["fitness"][-1] * gambiarra)
     
     best_fits = np.array(best_fits, dtype=np.float64)
-    print(f"{func} -> mean: {best_fits.mean()/gambiarra}\tstd: {best_fits.std()/gambiarra}")
+    print(f"{func} -> mean: {best_fits.mean()/gambiarra}    \tstd: {best_fits.std()/gambiarra}")
